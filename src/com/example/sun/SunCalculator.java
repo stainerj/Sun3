7b8cc823ee49bd5cbae921dd233f5a531963201d
stainerj <jstainer@outlook.com>
2022-02-01 18:12:48 +0000

    SunCalculator output shortened. Unused println statements removed. Unformatted numbers formatted to 4 decimal places.

--- src/com/example/sun/SunCalculator.java
+++ src/com/example/sun/SunCalculator.java
@@ -8,6 +8,7 @@ public class SunCalculator {
     public static Scanner keyboard = new Scanner(System.in);//input stream
 
     private static final DecimalFormat df2 = new DecimalFormat("00.00");
+    private static final DecimalFormat df4 = new DecimalFormat("0.0000");
     private static final DecimalFormat dfw = new DecimalFormat("##");
     private static final DecimalFormat dfm = new DecimalFormat("00");
     private static final DecimalFormat dfn = new DecimalFormat("0");
@@ -32,6 +33,7 @@ public class SunCalculator {
     public static double jSet = 0;//sunset time (Julian date)
     public static double hourAngle = 0;
     public static double timeZone = 0;
+    public static double equationOfCentre = 0;
 
     public static final double J2K = 2451545;//J2000
     public static final double ELOP = 102.9373;//ecliptic longitude of perihelion
@@ -46,15 +48,16 @@ public class SunCalculator {
 
     //main method
     public static void main(String[] args) {
-        System.out.println("________________________________________________________________________________\n" +
-                "\n" +
-                "   ___|                  ___|         |               |         |                  \n" +
-                " \\___ \\   |   |   _ \\   |       _` |  |   __|  |   |  |   _` |  __|   _ \\    __| \n" +
-                "       |  |   |  |   |  |      (   |  |  (     |   |  |  (   |  |    (   |  |    \n" +
-                " _____/  \\__,_| _|  _| \\____| \\__,_| _| \\___| \\__,_| _| \\__,_| \\__| \\___/  _|  \n" +
-                "\n" +
-                "SunCalculator (v0.0.1) \n" +
-                "________________________________________________________________________________");
+        System.out.println("""
+                ________________________________________________________________________________
+
+                   ___|                  ___|         |               |         |                 \s
+                 \\___ \\   |   |   _ \\   |       _` |  |   __|  |   |  |   _` |  __|   _ \\    __|\s
+                       |  |   |  |   |  |      (   |  |  (     |   |  |  (   |  |    (   |  |   \s
+                 _____/  \\__,_| _|  _| \\____| \\__,_| _| \\___| \\__,_| _| \\__,_| \\__| \\___/  _| \s
+
+                SunCalculator (v0.0.1)\s
+                ________________________________________________________________________________""");
         System.out.println();
         do {
             getDate();
@@ -62,11 +65,16 @@ public class SunCalculator {
             getPlace();
             getTimeZone();
             dateTimeToJulianDay();
-            System.out.println("LATITUDE/LONGITUDE ------------------------------------------------------------ " + df2.format(latitude) + ", " + df2.format(-longitude));
+            System.out.println("LATITUDE/LONGITUDE ------------------------------------ " + df2.format(latitude) + ", " + df2.format(-longitude));
             meanAnomaly();
             equationCentre();
             eclipticalCoordinates();
             equatorialCoordinates();
+            System.out.println("EQUATION OF CENTRE ------------------------------------ " + df4.format(equationOfCentre));
+            System.out.println("TRUE ANOMALY ------------------------------------------ " + df4.format(trueAnomaly));
+            System.out.println("ECLIPTIC LONGITUDE OF SUN ----------------------------- " + df4.format(eclipticLongSun));
+            System.out.println("SUN'S RIGHT ASCENSION --------------------------------- " + df4.format(rASun));
+            System.out.println("SUN'S DECLINATION ------------------------------------- " + df4.format(decSun));
             observer();
             solarTransit();//repeat stages for calculated value of jTransit to increase accuracy ~ meanAnomalyFromJulianDayTime(jTransit);
             solarTransitTime();
@@ -138,12 +146,12 @@ public class SunCalculator {
 
         julianDayRaw = c + day + e + f - 1524.5;
 
-        System.out.println("DATE/TIME --------------------------------------------------------------------- " + dfw.format(yearDisplay) + "/"
+        System.out.println("DATE/TIME --------------------------------------------- " + dfw.format(yearDisplay) + "/"
                 + dfw.format(monthDisplay) + "/" + dfw.format(day) + " " + df2.format(timeDay) + " (local)");
-        System.out.println("JULIAN DAY (DATE ONLY) -------------------------------------------------------- " + julianDayRaw);
+        System.out.println("JULIAN DAY (DATE ONLY) -------------------------------- " + julianDayRaw);
 
         julianDay = julianDayRaw + decTime;
-        System.out.println("EXACT JULIAN DAY NUMBER (INCLUDING TIME) -------------------------------------- " + julianDay);
+        System.out.println("EXACT JULIAN DAY NUMBER (INCLUDING TIME) -------------- " + df4.format(julianDay));
 
         //compensate for time zone offset
         julianDay = julianDay - (timeZone/24);
@@ -154,15 +162,13 @@ public class SunCalculator {
         double theta= M0 + M1*(julianDay - J2K);
         meanAnomDeg = theta%360;
 
-        System.out.println("MEAN ANOMALY, EARTH (DEGREES) ------------------------------------------------- " + meanAnomDeg);
+        System.out.println("MEAN ANOMALY, EARTH (DEGREES) ------------------------- " + df4.format(meanAnomDeg));
     }
 
     //calculate the mean anomaly in degrees
     public static double meanAnomalyFromJulianDayTime(double julianDayTime) {
         double theta= M0 + M1*(julianDayTime - J2K);
         meanAnomDeg = theta%360;
-
-        System.out.println("TRANSIT MEAN ANOMALY, EARTH (DEGREES) ----------------------------------------- " + meanAnomDeg);
         return meanAnomDeg;
     }
 
@@ -174,19 +180,14 @@ public class SunCalculator {
 
         //convert to radians
         double meanAnomRad = degreesToRadians(meanAnomDeg);
-        double equationOfCentre = (C1 * Math.sin(meanAnomRad)) + (C2 * Math.sin(2 * meanAnomRad)) + (C3 * Math.sin(3 * meanAnomRad));
+        equationOfCentre = (C1 * Math.sin(meanAnomRad)) + (C2 * Math.sin(2 * meanAnomRad)) + (C3 * Math.sin(3 * meanAnomRad));
         trueAnomaly = meanAnomDeg + equationOfCentre;
-
-        System.out.println("EQUATION OF CENTRE ------------------------------------------------------------ " + equationOfCentre);
-        System.out.println("TRUE ANOMALY ------------------------------------------------------------------ " + trueAnomaly);
     }
 
     //calculate the ecliptic longitude of the sun
     public static void eclipticalCoordinates() {
         double lambda = trueAnomaly + ELOP + 180;
         eclipticLongSun = lambda%360;
-
-        System.out.println("ECLIPTIC LONGITUDE OF SUN ----------------------------------------------------- " + eclipticLongSun);
     }
 
     //calculate the right ascension and declination of the sun
@@ -210,8 +211,6 @@ public class SunCalculator {
         decSun = Math.asin(sinLambdaSun*Math.sin(degreesToRadians(OOTE)));
         decSun = radiansToDegrees(decSun);
 
-        System.out.println("SUN'S RIGHT ASCENSION (ALTERNATIVE METHOD) --------- " + rASun);
-        System.out.println("SUN'S DECLINATION (ALTERNATIVE METHOD) ------------- " + decSun);
         //are alternative method values more accurate? Practically the same. Assume full equations are slightly better.
     }
 
@@ -221,8 +220,7 @@ public class SunCalculator {
         double sidereal = thetaSidereal%360; //sidereal time
         hourAngle = sidereal - rASun;
 
-        System.out.println("SIDEREAL TIME (DEGREES) ------------------------------------------------------- " + sidereal);
-        System.out.println("HOUR ANGLE H ------------------------------------------------------------------ " + hourAngle);
+        System.out.println("SIDEREAL TIME (DEGREES) ------------------------------- " + df4.format(sidereal));
 
         //convert to radians
         double hourAngleRad = degreesToRadians(hourAngle);
@@ -233,8 +231,8 @@ public class SunCalculator {
 
         double altitude = Math.asin(Math.sin(latitudeRad)*Math.sin(decSunRad) + Math.cos(latitudeRad)*Math.cos(decSunRad)* Math.cos(hourAngleRad));
         altitude = radiansToDegrees(altitude); //convert from radians to degrees
-        System.out.println("AZIMUTH ----------------------------------------------------------------------- " + azimuth);
-        System.out.println("ALTITUDE ---------------------------------------------------------------------- " + altitude);
+        System.out.println("SUN AZIMUTH ------------------------------------------- " + df4.format(azimuth));
+        System.out.println("SUN ALTITUDE ------------------------------------------ " + df4.format(altitude));
         //azimuth 0 = north
 
         if (altitude >= 0) {
@@ -287,26 +285,18 @@ public class SunCalculator {
 
         //repetition method to increase accuracy ~ jTransit = jTransit - hourAngleForJTransit/360
         do {
-            System.out.println();
-            System.out.println("RECALCULATE HOUR ANGLE H (REPETITION METHOD)");
             meanAnomalyFromJulianDayTime(jTransit);
             equationCentre();
             eclipticalCoordinates();
             equatorialCoordinates();
             hourAngleFromJulianDayTime(jTransit);
-
-            System.out.println("NEW HOUR ANGLE H -------------------------------------------------------------- " + hourAngle);// should reduce/tend to zero with iterations
-            System.out.println("TIME OF TRANSIT (JULIAN DAY) -------------------------------------------------- " + jTransit);
             jTransit = jTransit - (hourAngle/360);
-            System.out.println("TIME OF TRANSIT BY REPETITION METHOD ------------------------------------------ " + jTransit);
         } while (hourAngle > 0.0001);//close to zero
     }
 
     //calculate transit time (hours & minutes)
     public static void solarTransitTime() {
-        System.out.println();
-        System.out.println("TIME OF TRANSIT (HOURS AND MINUTES) ------------------------------------------- " + julianDayToTime(jTransit, julianDayRaw));
-        System.out.println();
+        System.out.println("TIME OF TRANSIT (HOURS AND MINUTES) ------------------- " + julianDayToTime(jTransit, julianDayRaw));
     }
 
     //calculate sunrise & sunset times
@@ -319,19 +309,12 @@ public class SunCalculator {
         double H =  Math.acos((Math.sin(degreesToRadians(H0)) - (Math.sin(degreesToRadians(latitude)) *
                 Math.sin(degreesToRadians(decSun))))/(Math.cos(degreesToRadians(latitude)) *
                 Math.cos(degreesToRadians(decSun))));
-        System.out.println("VALUE OF H (DEGREES) ---------------------------------------------------------- " + radiansToDegrees(H));
 
         //Jrise = jTransit - H/360 (*J3=1)
         jRise = jTransit - (radiansToDegrees(H)/360);//repetition method available     //first estimate
-        System.out.println("TIME OF SUNRISE (JULIAN DAY) ---------FIRST ESTIMATE--------------------------- " + jRise);
-        System.out.println("sunrise (HOURS AND MINUTES) first estimate ------------------------------------------- " + julianDayToTime(jRise, julianDayRaw));
 
         //start do while loop for repetition method here (sunrise)
         do {
-            System.out.println("                   ");
-            System.out.println("    rise ↑         ");
-            System.out.println("                   ");
-            System.out.println("RECALCULATE H & DECSUN for JRISE (REPETITION METHOD)");
             meanAnomalyFromJulianDayTime(jRise);
             equationCentre();
             eclipticalCoordinates();
@@ -340,26 +323,16 @@ public class SunCalculator {
             double hRise =  Math.acos((Math.sin(degreesToRadians(H0)) - (Math.sin(degreesToRadians(latitude)) *
                     Math.sin(degreesToRadians(decSun))))/(Math.cos(degreesToRadians(latitude)) *
                     Math.cos(degreesToRadians(decSun))));
-            System.out.println("VALUE OF NEW HRISE (DEGREES) -------------------REPETITION METHOD-------------- " + radiansToDegrees(hRise));
             //Jrise = jTransit - H/360 (*J3=1)
             jRise = jTransit - (radiansToDegrees(hRise)/360);//repetition method     //repeat estimate
-            System.out.println("TIME OF SUNRISE (JULIAN DAY) ---------REPEAT ESTIMATE-------------------------- " + jRise);
             correction = ((hRise - H)/360);
-            System.out.println("CORRECTION = " + correction);
-            System.out.println("                   ");
         } while (correction > 0.0001 || correction < -0.0001);
 
         //Jset = jTransit + H/360 (*J3=1)
         jSet = jTransit + (radiansToDegrees(H)/360);//repetition method available     //first estimate
-        System.out.println("TIME OF SUNSET (JULIAN DAY) ---------------FIRST ESTIMATE---------------------- " + jSet);
-        System.out.println("sunset (HOURS AND MINUTES) first estimate ------------------------------------------- " + julianDayToTime(jSet, julianDayRaw));
 
         //start do while loop for repetition method here (sunset)
         do {
-            System.out.println("                   ");
-            System.out.println("     set ↓         ");
-            System.out.println("                   ");
-            System.out.println("RECALCULATE H & DECSUN for JSET (REPETITION METHOD)");
             meanAnomalyFromJulianDayTime(jSet);
             equationCentre();
             eclipticalCoordinates();
@@ -368,19 +341,15 @@ public class SunCalculator {
             double hSet =  Math.acos((Math.sin(degreesToRadians(H0)) - (Math.sin(degreesToRadians(latitude)) *
                     Math.sin(degreesToRadians(decSun))))/(Math.cos(degreesToRadians(latitude)) *
                     Math.cos(degreesToRadians(decSun))));
-            System.out.println("VALUE OF NEW HSET (DEGREES) -------------------REPETITION METHOD--------------- " + radiansToDegrees(hSet));
             //Jrise = jTransit - H/360 (*J3=1)
             jSet = jTransit + (radiansToDegrees(hSet)/360);//repetition method     //repeat estimate
-            System.out.println("TIME OF SUNSET (JULIAN DAY) ---------REPEAT ESTIMATE--------------------------- " + jSet);
             correction = ((hSet - H)/360);
-            System.out.println("CORRECTION = " + correction);
         } while (correction > 0.0001 || correction < -0.0001);
     }
 
     public static void riseAndSetTime() {
-        System.out.println("  ");
-        System.out.println("TIME OF SUNRISE (HOURS AND MINUTES) ------------------------------------------- " + julianDayToTime(jRise, julianDayRaw));
-        System.out.println("TIME OF SUNSET (HOURS AND MINUTES) -------------------------------------------- " + julianDayToTime(jSet, julianDayRaw));
+        System.out.println("TIME OF SUNRISE (HOURS AND MINUTES) ------------------- " + julianDayToTime(jRise, julianDayRaw));
+        System.out.println("TIME OF SUNSET (HOURS AND MINUTES) -------------------- " + julianDayToTime(jSet, julianDayRaw));
     }
 
     //convert Julian day number to time of day in hours and minutes
